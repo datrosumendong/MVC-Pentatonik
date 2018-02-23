f34f01a051ac82103a8b9323d477e3f359c44d63
Datros Sumendong <dsumendong@gmail.com>
2018-02-23 21:35:31 +0800

    Struktur File dan Folder MVC Pentatonik

--- app/views/models_view/index/index.php
+++ app/views/models_view/index/index.php
@@ -4,9 +4,10 @@
       <a href="<?=URL;?>"><h1><?=TITLE;?></h1></a>
     </header>
     <nav>
-      <a href="#">home</a> /
-      <a href="#">dokumentasi</a> /
-      <a href="#">tentang</a>
+      <a href="https://github.com/datrosumendong/MVC-Pentatonik/wiki">home</a> /
+      <a href="https://github.com/datrosumendong/MVC-Pentatonik/wiki/Dokumentasi">dokumentasi</a> /
+      <a href="https://github.com/datrosumendong/MVC-Pentatonik/wiki/Database">database</a> /
+      <a href="https://datrosumendong.github.io">kontak</a>
     </nav>
   </div>
 </div>
