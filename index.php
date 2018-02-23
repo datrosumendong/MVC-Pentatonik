baa20a2a7b5181bbe1aa3a5d772a3f31e328949b
Datros Sumendong <dsumendong@gmail.com>
2018-02-24 07:17:19 +0800

    Struktur File dan Folder MVC Pentatonik

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
 
   /**
-   * @config file
+   * @Set Default Time Zone
    */
-  include 'config/config.php';
-
-  /**
-   * @All Function
-   */
-  include 'pentatonik/func/allfunction.php';
-
-  /**
-   * @Spl_autoload_register
-   */
-  include 'config/auto_load.php';
+  date_default_timezone_set('Asia/Makassar');
 
   /**
    * @START SESSION
    */
   session_start();
+
+  /**
+   * @config file
+   */
+  require_once 'config/config.php';
+
+  /**
+   * @All Function
+   */
+  require_once 'pentatonik/func/allfunction.php';
+
+  /**
+   * @Spl_autoload_register
+   */
+  require_once 'config/auto_load.php';
 
   /**
    * @Memulai routing controllers
