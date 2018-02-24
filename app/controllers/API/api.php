f1872880c9e7f4850dd3d424a656e89e1ba3fe49
Datros Sumendong <dsumendong@gmail.com>
2018-02-24 16:12:35 +0800

    Struktur File dan Folder MVC Pentatonik

---
+++ app/controllers/API/api.php
@@ -0,0 +1,17 @@
+<?php
+
+  class api extends controller
+  {
+    public function __construct()
+    {
+      echo 'api.php <br/>';
+      // $this->loadModel('api');
+    }
+
+    public function api()
+    {
+      // $this->model->api();
+    }
+  }
+
+?>
