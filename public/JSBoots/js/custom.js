ae31bd62c54340845010b181abb7cd4e9e1d5ffa
Datros Sumendong <dsumendong@gmail.com>
2018-02-26 19:08:47 +0800

    Struktur File dan Folder MVC Pentatonik

--- public/JSBoots/js/custom.js
+++ public/JSBoots/js/custom.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
-  console.log("%cWARNING!!!", "color: red; font-size: 80px; font-family:tahoma; font-weight:bold");
-  console.log("%cNKRI HARGA MATI", "color: red; font-size: 150px; font-family:tahoma; font-weight:bold");
-});
+// Mark Console Browser
+console.log("%cWARNING!!!", "color: red; font-size: 80px; font-family:tahoma; font-weight:bold");
+console.log("%cNKRI HARGA MATI", "color: red; font-size: 150px; font-family:tahoma; font-weight:bold");
